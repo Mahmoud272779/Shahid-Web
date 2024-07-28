2debfcc87e13921cd5ce590497c1e3898f02ea3b
Mahmoud El-Sayed <me974931@gmail.com>
2024-07-28 15:47:51 +0300

    search complete

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import NavBar from './components/NavBar'
 import MoviesList from './components/MoviesList';
 
 function App() {
-  const [movies,setmovies]=useState([])
+const [movies,setmovies]=useState([])
+const [isSearch,setisSearch]=useState(false)
   const getAllMovies=async ()=>{
  const res=await axios.get("https://api.themoviedb.org/3/person/popular?language=ar&page=1&api_key=675e2d4e494c3d2d212e5237fe047d61")
  setmovies(res.data.results)
+ setisSearch(false)
  
 }
 
@@ -17,6 +19,7 @@ const getSearchedMovies=async (word)=>{
     return getAllMovies()
   const res=await axios.get(`https://api.themoviedb.org/3/search/movie?query=`+word+"&api_key=675e2d4e494c3d2d212e5237fe047d61")
   setmovies(res.data.results)
+  setisSearch(true)
   
  }
   
@@ -28,8 +31,8 @@ useEffect(()=>{
 },[])
   return (<div>
 
-<NavBar search={getSearchedMovies}></NavBar>
-<MoviesList movies={movies}></MoviesList>
+<NavBar search={getSearchedMovies } ></NavBar>
+<MoviesList movies={movies} isSearchParam={isSearch}></MoviesList>
   </div>)
     
 }
--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -2,11 +2,11 @@ import React from "react";
 import { Row, Card, Col } from "react-bootstrap";
 import CardMovie from "./CardMovie";
 //import PaginationComponent from './Pagination'
-const MoviesList = ({ movies, getPage, pageCount }) => {
+const MoviesList = ({ movies, getPage, pageCount,isSearchParam }) => {
   return (
     <Row className="mt-3">
       {movies.length >= 1 ? (movies.map((mov) => {
-        return (<CardMovie key={mov.id} mov={mov} />)
+        return (<CardMovie key={mov.id} mov={mov} SP={isSearchParam} />)
       })) : <h2 className="text-center p-5">لا يوجد افلام...</h2>}
 
       
